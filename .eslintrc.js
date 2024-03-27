a7a6f34981542c35841ec4560a893431fb84d45e
Mateo Le Fur <mateo.le-fur@hotmail.com>
2024-03-27 14:07:59 +0100

    feat: setup eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,183 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      sourceType: 'module',
+    },
+    plugins: [
+      'eslint-plugin-jsdoc',
+      'eslint-plugin-import',
+      '@typescript-eslint',
+    ],
+    root: true,
+    rules: {
+      '@typescript-eslint/adjacent-overload-signatures': 'error',
+      '@typescript-eslint/array-type': [
+        'error',
+        {
+          default: 'array',
+        },
+      ],
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/ban-types': [
+        'error',
+        {
+          types: {
+            Object: {
+              message: 'Avoid using the `Object` type. Did you mean `object`?',
+            },
+            Function: {
+              message:
+                'Avoid using the `Function` type. Prefer a specific function type, like `() => void`.',
+            },
+            Boolean: {
+              message:
+                'Avoid using the `Boolean` type. Did you mean `boolean`?',
+            },
+            Number: {
+              message: 'Avoid using the `Number` type. Did you mean `number`?',
+            },
+            String: {
+              message: 'Avoid using the `String` type. Did you mean `string`?',
+            },
+            Symbol: {
+              message: 'Avoid using the `Symbol` type. Did you mean `symbol`?',
+            },
+            '{}': false,
+          },
+        },
+      ],
+      '@typescript-eslint/consistent-type-assertions': 'error',
+      '@typescript-eslint/dot-notation': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': [
+        'error',
+        {
+          allowArgumentsExplicitlyTypedAsAny: true,
+          allowDirectConstAssertionInArrowFunctions: true,
+          allowHigherOrderFunctions: false,
+          allowTypedFunctionExpressions: false,
+        },
+      ],
+      '@typescript-eslint/no-empty-function': 'error',
+      '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-new': 'error',
+      '@typescript-eslint/no-namespace': 'error',
+      '@typescript-eslint/no-parameter-properties': 'off',
+      '@typescript-eslint/no-shadow': [
+        'error',
+        {
+          hoist: 'all',
+        },
+      ],
+      '@typescript-eslint/no-unused-expressions': 'error',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/prefer-for-of': 'error',
+      '@typescript-eslint/prefer-function-type': 'error',
+      '@typescript-eslint/prefer-namespace-keyword': 'error',
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/quotes': [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
+      '@typescript-eslint/triple-slash-reference': [
+        'error',
+        {
+          path: 'always',
+          types: 'prefer-import',
+          lib: 'always',
+        },
+      ],
+      '@typescript-eslint/typedef': [
+        'error',
+        {
+          parameter: true,
+          propertyDeclaration: true,
+          memberVariableDeclaration: true,
+        },
+      ],
+      '@typescript-eslint/unified-signatures': 'error',
+      complexity: [
+        'error',
+        {
+          max: 12,
+        },
+      ],
+      'constructor-super': 'error',
+      'dot-notation': 'error',
+      eqeqeq: ['error', 'always'],
+      'guard-for-in': 'error',
+      'id-denylist': 'error',
+      'id-match': 'error',
+      'import/no-default-export': 'error',
+      'jsdoc/check-alignment': 'error',
+      'jsdoc/check-indentation': 'error',
+      'jsdoc/newline-after-description': 'error',
+      'max-classes-per-file': ['error', 3],
+      'max-len': [
+        'error',
+        {
+          code: 180,
+        },
+      ],
+      'new-parens': 'error',
+      'no-bitwise': 'error',
+      'no-caller': 'error',
+      'no-cond-assign': 'error',
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'no-else-return': 'error',
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': ['error'],
+      'no-empty': 'error',
+      'no-empty-function': ['error', { allow: ['constructors'] }],
+      'no-eval': 'error',
+      'no-fallthrough': 'off',
+      'no-invalid-this': 'off',
+      'no-magic-numbers': ['error', { ignore: [-1, 0, 1] }],
+      'no-new-wrappers': 'error',
+      'no-param-reassign': 'error',
+      'no-return-await': 'error',
+      'no-throw-literal': 'error',
+      'no-trailing-spaces': 'error',
+      'no-undef-init': 'error',
+      'jsdoc/newline-after-description': 'off',
+      'no-unsafe-finally': 'error',
+      'no-unused-expressions': 'error',
+      'no-unused-labels': 'error',
+      'no-unused-vars': 'off',
+      'no-use-before-define': 'off',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'one-var': ['error', 'never'],
+      'prefer-const': 'error',
+      'quote-props': ['error', 'as-needed'],
+      radix: 'error',
+      'space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          named: 'never',
+          asyncArrow: 'always',
+        },
+      ],
+      'space-in-parens': ['error', 'never'],
+      'spaced-comment': [
+        'error',
+        'always',
+        {
+          markers: ['/'],
+        },
+      ],
+      'use-isnan': 'error',
+      'valid-typeof': 'off',
+    },
   },
 };

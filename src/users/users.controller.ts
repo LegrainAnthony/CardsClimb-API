ed04032bc2e326c3ffa1b3f00ff7d5387d745af4
Mateo Le Fur <mateo.le-fur@hotmail.com>
2024-03-28 11:14:57 +0100

    :sparkles: add example

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
+// The @Controller() decorator takes a string argument that defines the route for the controller.
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // The ParseIntPipe pipe is used to transform the id parameter from a string to a number.
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  // The DTO class is used to define the shape of the data that the client should send to the server.
+  @Post()
+  create(@Body() user: CreateUserDto) {
+    return this.usersService.create(user);
+  }
+}

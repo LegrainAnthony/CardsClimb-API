2d5c18ca055efd558b3e97e596c5c6c194cb2f31
BenjaminAdrien <benadrien314@gmail.com>
2024-03-29 11:49:34 +0100

    add method to remove color_id key to object data

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,6 +1,6 @@
+import { UpdateTagDto } from './dto/update-tag.dto';
 import { Injectable } from '@nestjs/common';
 import { TagRepository } from './tags.repository';
-import { Prisma } from '@prisma/client';
 import { CreateTagDto } from './dto/create-tag.dto';
 
 @Injectable()
@@ -17,7 +17,7 @@ export class TagsService {
 
   create(data: CreateTagDto) {
     return this.tagRepository.create({
-      name: data.name,
+      ...(dataToSend(data) as CreateTagDto),
       color: {
         connect: {
           id: data.color_id,
@@ -26,11 +26,35 @@ export class TagsService {
     });
   }
 
-  update(id: number, data: Prisma.TagUpdateInput) {
-    return this.tagRepository.update({ id }, data);
+  update(id: number, data: UpdateTagDto) {
+    return this.tagRepository.update(
+      { id },
+      {
+        ...(dataToSend(data) as UpdateTagDto),
+        color: {
+          connect: {
+            id: data.color_id,
+          },
+        },
+      },
+    );
   }
 
   delete(id: number) {
     return this.tagRepository.delete({ id });
   }
+}
+
+/**
+ * Map un objet pour qu'il corresponde au format demandé par Prisma pour les Tags
+ * @param data Objet à mapper
+ * @returns
+ */
+function dataToSend(data: Object) {
+  return Object.keys(data)
+    .filter((key) => key != 'color_id')
+    .reduce((obj, key) => {
+      obj[key] = data[key];
+      return obj;
+    }, {});
 }

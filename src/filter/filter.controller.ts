df8ca6d3a304db4a1bbe7782510f39cbbf4d8469
LegrainAnthony <legrain.anthony51@gmail.com>
2024-05-10 11:21:38 +0200

    :sparkles: cards properties route

--- src/filter/filter.controller.ts
+++ src/filter/filter.controller.ts
@@ -1,15 +1,10 @@
 import {
-  Body,
   Controller,
   Get,
-  ParseBoolPipe,
-  ParseIntPipe,
-  Query,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilterService } from './filter.service';
-import { ActiveUser } from 'src/common/decorators';
-import { CardFilterDto } from './dto/cards-filter.dto';
+import { Public } from 'src/common/decorators';
 import { ParseBigIntInterceptor } from 'src/common/interceptor/parse-bigint.interceptor';
 import { prismaClientHandler } from 'src/common/interceptor';
 
@@ -18,20 +13,9 @@ import { prismaClientHandler } from 'src/common/interceptor';
 export class FilterController {
   constructor(private readonly filterService: FilterService) {}
 
-  // ! à supprimé
-
-  @Get('blitz')
-  CardFilter(
-    @Body() data: CardFilterDto,
-    @ActiveUser() userId: number,
-    @Query('randomResult', ParseBoolPipe) randomResult: boolean,
-    @Query('numberOfCard', ParseIntPipe) numberOfCard: number,
-  ) {
-    return this.filterService.cardFilter(
-      data,
-      userId,
-      randomResult,
-      numberOfCard,
-    );
+  @Public()
+  @Get('card-properties')
+  cardsFilterProperties() {
+    return this.filterService.CardFilterProperties();
   }
 }
